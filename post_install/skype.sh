73d4fadacf363a3d5296ad815f0b5abb5510c1e6
h.ozgur cagdas <ocagdas@zmartsolutions.com>
2021-12-21 15:50:29 +0000

    install skype with dpkg

--- post_install/skype.sh
+++ post_install/skype.sh
@@ -8,7 +8,7 @@ fi
 source ./common_bash_funcs.sh
 
 wget https://repo.skype.com/latest/skypeforlinux-64.deb
-sudo gdebi --non-interactive skypeforlinux-64.deb
+sudo dpkg -i skypeforlinux-64.deb
 rm -f skypeforlinux-64.deb
 
 func_print_info_message "script end `basename "$0"`"
